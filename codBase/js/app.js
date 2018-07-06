94809da6602ae34104bf22e050edc1d81338a823
jorgeandres6 <jorgeandres6@msn.com>
2018-07-05 22:44:33 -0500

    Falta encadenar

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -1,77 +1,129 @@
-var rep = true;
-var oper = "";
-var num1 = 0;
+var rep = true; //inicializacion variable para ingreso de punto
+var oper = ""; //incializacion variable para escoger operacion aritmetica
+var num = 0; // inicializacion variable para almacenar primer operando de la operacion matematica
+var numaux1 = 0;
+var numaux2 = 0;
+var numresp = 0;
+var segoper =  true;
 
-document.getElementById(0).onclick = function () {presion(this.id)};
-document.getElementById(1).onclick = function () {presion(this.id)};
-document.getElementById(2).onclick = function () {presion(this.id)};
-document.getElementById(3).onclick = function () {presion(this.id)};
-document.getElementById(4).onclick = function () {presion(this.id)};
-document.getElementById(5).onclick = function () {presion(this.id)};
-document.getElementById(6).onclick = function () {presion(this.id)};
-document.getElementById(7).onclick = function () {presion(this.id)};
-document.getElementById(8).onclick = function () {presion(this.id)};
-document.getElementById(9).onclick = function () {presion(this.id)};
-document.getElementById("punto").onclick = function () {presion(this.id)};
-document.getElementById("on").onclick = function () {presion(this.id)};
-document.getElementById("sign").onclick = function () {presion(this.id)};
-document.getElementById("por").onclick = function () {operacion(this.id)};
-document.getElementById("dividido").onclick = function () {operacion(this.id)};
-document.getElementById("menos").onclick = function () {operacion(this.id)};
-document.getElementById("mas").onclick = function () {operacion(this.id)};
-document.getElementById("igual").onclick = function () {fin(oper,num1)};
+//Eventos on click
+document.getElementById(0).onmousedown = function () {presion(this.id)};
+document.getElementById(1).onmousedown = function () {presion(this.id)};
+document.getElementById(2).onmousedown = function () {presion(this.id)};
+document.getElementById(3).onmousedown = function () {presion(this.id)};
+document.getElementById(4).onmousedown = function () {presion(this.id)};
+document.getElementById(5).onmousedown = function () {presion(this.id)};
+document.getElementById(6).onmousedown = function () {presion(this.id)};
+document.getElementById(7).onmousedown = function () {presion(this.id)};
+document.getElementById(8).onmousedown = function () {presion(this.id)};
+document.getElementById(9).onmousedown = function () {presion(this.id)};
+document.getElementById("punto").onmousedown = function () {presion(this.id)};
+document.getElementById("on").onmousedown = function () {presion(this.id)};
+document.getElementById("sign").onmousedown = function () {presion(this.id)};
+document.getElementById("por").onmousedown = function () {operacion(this.id)};
+document.getElementById("dividido").onmousedown = function () {operacion(this.id)};
+document.getElementById("menos").onmousedown = function () {operacion(this.id)};
+document.getElementById("mas").onmousedown = function () {operacion(this.id)};
+document.getElementById("igual").onmousedown = function () {fin(this.id)};
+
+//funcion de presionar tecla
 
 function presion (id){
-  if (id == "punto" && rep) {
+
+  if (id == "punto" && rep) { //presionar punto
     acum('.');
     rep = false;
   }
-  else if (id == "on") {
+  else if (id == "on") { //presionar tecla on
     document.getElementById("display").innerHTML = 0;
     rep = true;
   }
-  else if (id != "punto" && id != "on" && id != "sign" && id != "raiz" && id != "dividido" && id != "por" && id != "menos" && id != "igual" && id != "mas") {
+  else if (id != "punto" && id != "on" && id != "sign" && id != "raiz" && id != "dividido" && id != "por" && id != "menos" && id != "igual" && id != "mas") { // presionar tecla numerica
+    if (document.getElementById("display").innerHTML == '0') {// comprobación de cero en pantalla
+      document.getElementById("display").innerHTML = "";
+    }
     var pres = document.getElementById(id).id;
     acum(pres);
   }
-  else if (id == "sign"){
+  else if (id == "sign"){ //presionar tecla cambio de signo
     var num = document.getElementById("display").innerHTML;
     num = num * -1;
     document.getElementById("display").innerHTML = num;
   }
-  else {
+  else if (id != "punto") { //presionar tecla de operacion aritmetica
     operacion();
+    if (!segoper) {
+      oper2 (oper, numaux1, numaux2);
+    }
   }
+
+  resize(id);
+
 }
 
-function acum (x){
+function acum (x){ //encadenar caracteres en pantalla
+  var y = document.getElementById("display").innerHTML;
+  num = y+x;
+  console.log(num);
   if (document.getElementById("display").innerHTML.length < 8) {
-    var y = document.getElementById("display").innerHTML;
     document.getElementById("display").innerHTML = y+x;
   }
 }
 
-function operacion (id){
-  num1 = document.getElementById("display").innerHTML;
+function operacion (id){ //guardar el numero en pantalla en una variable
+  rep = true;
+  if (segoper){
+    numaux1 = num;
+  }
+  else {
+    numaux2 = num;
+    oper2 (oper, numaux1, numaux2);
+  }
+  num = 0;
   document.getElementById("display").innerHTML = 0;
   oper=id;
+  segoper = false;
+
+  resize(id);
+
 }
 
-function fin (oper, num1){
-  var num2 = document.getElementById("display").innerHTML;
+function oper2 (oper, num1, num2){// operaciones artimeticas
+
   switch (oper) {
     case "mas":
-      document.getElementById("display").innerHTML = parseFloat(num1)+parseFloat(num2);
+      numresp = parseFloat(num1)+parseFloat(num2);
       break;
     case "menos":
-      document.getElementById("display").innerHTML = num1-num2;
+      numresp = num1-num2;
       break;
     case "por":
-      document.getElementById("display").innerHTML = num1*num2;
+      numresp = num1*num2;
       break;
     case "dividido":
-      document.getElementById("display").innerHTML = num1/num2;
+      numresp = num1/num2;
       break;
-    default:
   }
+  numaux1 = numresp;
+  //numaux2 = 0;
+}
+
+function fin (id){
+  numaux2 = num;
+  oper2 (oper, numaux1, numaux2)
+  var presentacion = String(numresp);
+  console.log(presentacion);
+  document.getElementById("display").innerHTML = "";
+  document.getElementById("display").innerHTML = presentacion.substring(0,7);
+  //num = 0;
+  segoper = true;
+  resize(id);
+}
+
+function resize (id){
+  var tanH = document.getElementById(id);
+  tanH.style.transform='scale(0.9)';
+  setTimeout(function(){
+    tanH.style.transform='scale(1)';
+  }, 100);
 }
